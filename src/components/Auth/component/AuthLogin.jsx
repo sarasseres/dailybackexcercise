dddf5800d565484d4afdabb89b6c6f7a3fc470ba
dsaraswati907 <dsaraswati907@gmail.com>
2023-09-30 08:50:28 +0700

    feat adding integration api at frontend auth

--- src/components/Auth/component/AuthLogin.jsx
+++ src/components/Auth/component/AuthLogin.jsx
@@ -1,16 +1,36 @@
 'use client';
-
+import { useState } from 'react';
 import { Input, Button } from '@nextui-org/react';
 
 export const AuthLogin = () => {
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+
+  const handleSubmitlogin = async () => {
+    const res = await fetch('api/v1/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(loginData),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <main className="h-screen w-full  grid grid-cols-2">
       <div className="bg-rose-500 "></div>
       <div className="flex justify-center items-center ">
         <div className="max-w-[320px] space-y-2">
-          <Input label="email" variant="bordered" placeholder="Enter your email" className="" />
-          <Input label="Password" variant="bordered" placeholder="Enter your password" className="" />
-          <Button className="">Login</Button>
+          <Input name="email" label="email" variant="bordered" placeholder="Enter your email" className="" onChange={handleChange} />
+          <Input name="password" label="Password" variant="bordered" placeholder="Enter your password" className="" onChange={handleChange} />
+          <Button onClick={handleSubmitlogin} className="">
+            Login
+          </Button>
         </div>
       </div>
     </main>
--- src/components/Auth/component/AuthRegister.jsx
+++ src/components/Auth/component/AuthRegister.jsx
@@ -1,16 +1,38 @@
 'use client';
 import { Input, Button } from '@nextui-org/react';
+import { useState } from 'react';
 
 export const AuthRegister = () => {
+  const [registerData, setRegisterData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegisterData({ ...registerData, [name]: value });
+  };
+
+  const handleSubmitregister = async () => {
+    const res = await fetch('api/v1/auth/register', {
+      method: 'POST',
+      body: JSON.stringify(registerData),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <main className="h-screen w-full  grid grid-cols-2">
       <div className="bg-rose-500 "></div>
       <div className="flex justify-center items-center ">
         <div className="max-w-[320px] space-y-2">
-          <Input label="name" variant="bordered" placeholder="Enter your name" className="" />
-          <Input label="email" variant="bordered" placeholder="Enter your email" className="" />
-          <Input label="Password" variant="bordered" placeholder="Enter your password" className="" />
-          <Button className="">Register</Button>
+          <Input name="name" label="name" variant="bordered" placeholder="Enter your name" className="" onChange={handleChange} />
+          <Input name="email" label="email" variant="bordered" placeholder="Enter your email" className="" onChange={handleChange} />
+          <Input name="password" label="password" variant="bordered" placeholder="Enter your password" className="" type="password" onChange={handleChange} />
+          <Button className="" onClick={handleSubmitregister}>
+            Register
+          </Button>
         </div>
       </div>
     </main>
